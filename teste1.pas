68b5a23da2d92537ef4d69b622f8f54ab9fa278d
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2015-10-31 23:37:36 -0200

    Improve test file
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- teste1.pas
+++ teste1.pas
@@ -1,7 +1,24 @@
 program exemplo75 (input, output);
 var m,n :  integer;
 begin
-  m := 2;
+  m := (2 + 3) * 5;
   n := 3;
+
+  if true then
+  begin
+    n := 5;
+  end
+  else
+  begin
+    m := 19;
+  end;
+
+  while false do 
+  begin
+    while true do
+    begin
+      m := m + n;
+    end;
+  end;
 end.
 
