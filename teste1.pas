3f31faa04d9bcae087278084d25c8cf74bea2352
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2015-11-02 22:37:01 -0200

    Write tests for function and procedure declarations and calls
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- teste1.pas
+++ teste1.pas
@@ -1,10 +1,24 @@
 program exemplo75 (input, output);
-var m,n :  integer;
+
+var m, n :  integer;
+
+function func1() : integer;
+var t : integer;
+begin
+  t := 5;
+end
+
+procedure proc1();
+var k : integer;
+begin
+  k := 15 + 2;
+end
+
 begin
   m := (2 + 3) * 5;
   n := 3;
 
-  if true then
+  if m < 3 then
   begin
     n := 5;
   end
@@ -13,12 +27,15 @@ begin
     m := 19;
   end;
 
-  while false do 
+  while m < 1 do 
   begin
-    while true do
+    while m < n do
     begin
       m := m + n;
     end;
   end;
+
+  n := func1();
+  proc1();
 end.
 
